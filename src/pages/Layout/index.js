8870fd42d6d04503ea1ae5e42e694100e2453f54
songmengqiang <8280519+songmengqiang@user.noreply.gitee.com>
2026-09-20 16:11:57 +0800

    主题定制化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
@@ -5,6 +6,10 @@ export default function Layout() {
         <div>
             <Outlet />
             我是Layout
+            <Button color="primary">测试全局</Button>
+            <div className="puple">
+                <Button color="primary">测试局部</Button>
+            </div>
         </div>
     )
 }
